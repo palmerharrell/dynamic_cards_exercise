089cbf387e085bedf48ff583a1f4d44f293195f5
Palmer Harrell <palmerharrell@me.com>
2016-01-29 15:20:08 -0600

    Created some test cards and CSS

--- javascripts/cards.js
+++ javascripts/cards.js
@@ -9,3 +9,13 @@
 // cards, should then be removed from the DOM. Not just made invisible, actually 
 // removed from the DOM.
 
+var createButton = document.getElementsByTagName("button")[0];
+var cardContainer = document.getElementById("card_container");
+var cardTemplate = `<div class="card"><div class="content"></div><button>Delete</button></div>`
+var userInput = "";
+
+cardContainer.innerHTML += cardTemplate;
+cardContainer.getElementsByClassName("content")[0].innerHTML = "Testing Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem non veniam, nam quisquam aliquid maxime quia! Doloribus dolore unde, nisi nihil quaerat, consectetur rerum modi sint, repudiandae laborum delectus ullam.";
+
+cardContainer.innerHTML += cardTemplate;
+cardContainer.getElementsByClassName("content")[1].innerHTML = "Testing Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem non veniam, nam quisquam aliquid maxime quia! Doloribus dolore unde, nisi nihil quaerat, consectetur rerum modi sint, repudiandae laborum delectus ullam.";
